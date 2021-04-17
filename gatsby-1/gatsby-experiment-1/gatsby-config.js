cbc5cc16953f538164bdbd07a3fc9ffe9db0e985
Eoin Kelly <eoin@eoinkelly.info>
2021-04-17 13:19:12 +1200

    Add gatsby experiment

---
+++ gatsby-1/gatsby-experiment-1/gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby Experiment 1",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
